fb365571f1cdbdcda632f598b4e9d436a59063da
Stern <sternben@householder.cs.grinnell.edu>
2022-02-06 18:51:17 -0600

    idt in progress

--- kernel/strlib.c
+++ kernel/strlib.c
@@ -1,3 +1,14 @@
+#include <stddef.h>
+
+void* memset(void* s, int c, size_t n) {
+  int* mem_area = (int*) s;
+  for (int i = 0; i < n; i++) {
+    mem_area[i] = c;
+    mem_area++;
+  }
+  return s;
+}
+
 // Retuns the length of a string. (strlen)
 int stringlen(const char* str) {
   int result = 0;
--- kernel/strlib.h
+++ kernel/strlib.h
@@ -2,3 +2,6 @@
 
 // Retuns the length of a string. (strlen)
 int stringlen(const char* str);
+
+// Sets memory locations to a given int. Equivalent to memset().
+void* memset(void* s, int c, size_t n);
